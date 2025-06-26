3d5360375681bcbd9264d02fca5f47b9b1e9c94a
Arif Muamar <149848116+arifmuamardev@users.noreply.github.com>
2025-06-26 10:05:52 +0700

    fix: add image and correct background-image path

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -9,7 +9,7 @@ const EventList = () => {
 
   useEffect(() => {
     setEvents(eventData);
-     fetch("/data.json")
+    fetch("/data.json")
       .then((res) => {
         console.log("Response status:", res.status);
         return res.json();
@@ -20,38 +20,48 @@ const EventList = () => {
       })
       .catch((err) => {
         console.error("Fetch gagal:", err);
-      }); 
+      });
   }, []);
 
   const filteredEvents = events.filter((event) =>
     event.namaEvent.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  // style background image di sini
+  const divStyle = {
+    backgroundImage: `url(${require('../assets/images/gradasibg.jpg')})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    padding: '20px'
+  };
+
   return (
-    <div className="event-list-container">
-      <h1 className="judul-event">Event Kampus Tirta Persada</h1>
+    <div style={divStyle}>
+      <div className="event-list-container">
+        <h1 className="judul-event">Event Kampus Tirta Persada</h1>
 
-      <input
-        type="text"
-        placeholder="Cari event..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="search-box"
-      />
+        <input
+          type="text"
+          placeholder="Cari event..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-box"
+        />
 
-      <div className="event-cards-wrapper">
-        {filteredEvents.length > 0 ? (
-          filteredEvents.map((event) => (
-            <EventCard key={event.id} {...event} />
-          ))
-        ) : (
-          <p className="no-event">Tidak ada event yang cocok.</p>
-        )}
+        <div className="event-cards-wrapper">
+          {filteredEvents.length > 0 ? (
+            filteredEvents.map((event) => (
+              <EventCard key={event.id} {...event} />
+            ))
+          ) : (
+            <p className="no-event">Tidak ada event yang cocok.</p>
+          )}
+        </div>
       </div>
     </div>
   );
 };
-
-
 
 export default EventList;
