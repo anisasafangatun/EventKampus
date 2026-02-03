886bb99fb61b6fa40783fc46b467185e594eba3a
Anisa Safangatun <xaztrsanisa@gmail.com>
2026-02-03 10:06:46 +0700

    Build production

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,12 @@
 import Countdown from "./Countdown";
-import RegisterForm from "./Registerform";
+import Registerform from "./Registerform";
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Web Event Kampus</h1>
       <Countdown />
-      <RegisterForm />
+      <Registerform />
     </div>
   );
 }
--- frontend/src/components/Registerform.jsx
+++ frontend/src/components/Registerform.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function RegisterForm() {
+export default function Registerform() {
   const [form, setForm] = useState({
     nama: "",
     email: "",
